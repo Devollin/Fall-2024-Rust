468d9cf949cece5c7ca6096ed69055c17077e4ee
Devi <34558845+Devollin@users.noreply.github.com>
2024-08-28 15:31:43 +0000

    First Program Submission

--- HelloWorld/src/main.rs
+++ HelloWorld/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     // cargo new <NAME> makes a new project
-    // cargo run compiles and starts the program 
+    // cargo run compiles and starts the program
 
     let name = "Devi";
 

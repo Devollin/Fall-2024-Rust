c504ee8f3ab020ee4920ce21660ddb7d937d7deb
Devi <34558845+Devollin@users.noreply.github.com>
2024-09-09 14:23:25 +0000

    rust syntax exploration

--- HelloWorld/src/main.rs
+++ HelloWorld/src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
-    // cargo new <NAME> makes a new project
-    // cargo run compiles and starts the program
-
-    let name = "Devi";
-
-    println!("Hello, {}!", name);
+   // Converting a string to a number
+   let num = "25";
+   let num: u32 = num.parse()
+       .expect("Please provide a valid number!");
+   
+   println!("Parsed number: {}", num);
+   
+   // Further transformations
+   let num = num + 25;
+   let num = num * 2;
+   
+   println!("Final value of num: {}", num);
 }
